ad1271b2a89f8f3070624ad7a4d2a319a0f1ed19
Victor van den Hoven <victor.vanden.hoven@alliander.com>
2026-08-27 16:56:57 +0200

    Refactor OmnikPVOutputConfigFlow and options flow

--- custom_components/omnik_pvoutput/config_flow.py
+++ custom_components/omnik_pvoutput/config_flow.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 from typing import Any
 
 import voluptuous as vol
+from aiohttp import ClientError
 from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.core import callback
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
+from .api import OmnikPortalApi, validate_omnik_connection
 from .const import (
     CONF_INTERVAL,
     CONF_OMNIK_API_URL,
@@ -21,6 +23,10 @@ from .const import (
     DOMAIN,
 )
 
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
 
 class OmnikPVOutputConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -28,48 +34,89 @@ class OmnikPVOutputConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry: config_entries.ConfigEntry):
-        return OmnikPVOutputOptionsFlow(config_entry)
-
+        return OmnikPVOutputOptionsFlow()
+    
     async def async_step_user(self, user_input: dict[str, Any] | None = None):
-        if user_input is not None:
-            await self.async_set_unique_id(
-                f"{user_input[CONF_OMNIK_INVERTER]}-{user_input[CONF_OMNIK_INVERTER_ID]}"
-            )
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(
-                title=f"Omnik {user_input[CONF_OMNIK_INVERTER]}",
-                data=user_input,
-            )
+        errors: dict[str, str] = {}
 
-        schema = vol.Schema({
-            vol.Required(CONF_OMNIK_API_URL, default=DEFAULT_OMNIK_API_URL): str,
-            vol.Required(CONF_OMNIK_USERNAME): str,
-            vol.Required(CONF_OMNIK_PASSWORD): str,
-            vol.Required(CONF_OMNIK_INVERTER): str,
-            vol.Required(CONF_OMNIK_INVERTER_ID, default=1): vol.Coerce(int),
-            vol.Required(CONF_PVOUTPUT_API_KEY): str,
-            vol.Required(CONF_PVOUTPUT_SYSTEM_ID): str,
-            vol.Required(CONF_INTERVAL, default=DEFAULT_INTERVAL): vol.All(vol.Coerce(int), vol.Range(min=60)),
-        })
-        return self.async_show_form(step_id="user", data_schema=schema)
+        if user_input is not None:
+            try:
+                session = async_get_clientsession(self.hass)
+                api = OmnikPortalApi(
+                    session,
+                    user_input[CONF_OMNIK_API_URL],
+                    user_input[CONF_OMNIK_USERNAME],
+                    user_input[CONF_OMNIK_PASSWORD],
+                    user_input[CONF_OMNIK_INVERTER],
+                    int(user_input[CONF_OMNIK_INVERTER_ID]),
+                )
+                await validate_omnik_connection(api)
+            except (ClientError, TimeoutError) as err:
+                _LOGGER.exception("Could not connect to OmnikPortal: %s", err)
+                errors["base"] = "cannot_connect"
+
+            except RuntimeError as err:
+                _LOGGER.exception("OmnikPortal returned an error: %s", err)
+                errors["base"] = "invalid_auth"
+
+            except Exception as err:
+                _LOGGER.exception(
+                    "Unexpected error while validating OmnikPortal connection: %s",
+                    err,
+                )
+                errors["base"] = "unknown"
+
+
+            if not errors:
+                await self.async_set_unique_id(
+                    f"{user_input[CONF_OMNIK_INVERTER]}-{user_input[CONF_OMNIK_INVERTER_ID]}"
+                )
+                self._abort_if_unique_id_configured()
+                interval = user_input.pop(CONF_INTERVAL)
+                return self.async_create_entry(
+                    title=f"Omnik {user_input[CONF_OMNIK_INVERTER]}",
+                    data=user_input,
+                    options={CONF_INTERVAL: interval},
+                )
+
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_OMNIK_API_URL, default=DEFAULT_OMNIK_API_URL): str,
+                vol.Required(CONF_OMNIK_USERNAME): str,
+                vol.Required(CONF_OMNIK_PASSWORD): str,
+                vol.Required(CONF_OMNIK_INVERTER): str,
+                vol.Required(CONF_OMNIK_INVERTER_ID, default=1): vol.Coerce(int),
+                vol.Required(CONF_PVOUTPUT_API_KEY): str,
+                vol.Required(CONF_PVOUTPUT_SYSTEM_ID): str,
+                vol.Required(CONF_INTERVAL, default=DEFAULT_INTERVAL): vol.All(
+                    vol.Coerce(int), vol.Range(min=60)
+                ),
+            }
+        )
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
 
 class OmnikPVOutputOptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input: dict[str, Any] | None = None):
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ):
         if user_input is not None:
-            new_data = {**self.config_entry.data, CONF_INTERVAL: user_input[CONF_INTERVAL]}
-            self.hass.config_entries.async_update_entry(self.config_entry, data=new_data)
-            return self.async_create_entry(title="", data={})
+            return self.async_create_entry(title="", data=user_input)
+
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_INTERVAL,
+                    default=DEFAULT_INTERVAL,
+                ): vol.All(vol.Coerce(int), vol.Range(min=60)),
+            }
+        )
 
         return self.async_show_form(
             step_id="init",
-            data_schema=vol.Schema({
-                vol.Required(
-                    CONF_INTERVAL,
-                    default=self.config_entry.data.get(CONF_INTERVAL, DEFAULT_INTERVAL),
-                ): vol.All(vol.Coerce(int), vol.Range(min=60)),
-            }),
+            data_schema=self.add_suggested_values_to_schema(
+                schema,
+                self.config_entry.options,
+            ),
         )
+
